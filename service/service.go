ba810420d80f1508a4adc65e7cc05da383700381
Manfred Touron <m@42.am>
2016-12-14 20:25:38 +0100

    Add placeholder of the translator service implementation

---
+++ service/service.go
@@ -0,0 +1,18 @@
+package translatorsvc
+
+import (
+	"fmt"
+
+	"github.com/moul/translator/pb"
+	"golang.org/x/net/context"
+)
+
+type Service struct{}
+
+func New() translatorpb.TranslatorServiceServer {
+	return &Service{}
+}
+
+func (svc Service) Translate(ctx context.Context, input *translatorpb.TranslateRequest) (*translatorpb.TranslateResponse, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
