877f79105f2059fbe45783715477a640ac1e6d05
Manfred Touron <m@42.am>
2016-12-15 17:37:47 +0100

    Link gettext with the service

--- service/service.go
+++ service/service.go
@@ -1,18 +1,23 @@
 package translatorsvc
 
 import (
-	"fmt"
+	"github.com/chai2010/gettext-go/gettext"
+	"golang.org/x/net/context"
 
 	"github.com/moul/translator/gen/pb"
-	"golang.org/x/net/context"
 )
 
 type Service struct{}
 
 func New() translatorpb.TranslatorServiceServer {
+	gettext.BindTextdomain("translator", "locales", nil)
+	gettext.Textdomain("translator")
 	return &Service{}
 }
 
 func (svc Service) Translate(ctx context.Context, input *translatorpb.TranslateRequest) (*translatorpb.TranslateResponse, error) {
-	return nil, fmt.Errorf("Not implemented")
+	gettext.SetLocale(input.Language)
+	return &translatorpb.TranslateResponse{
+		Message: gettext.PGettext("", input.Message),
+	}, nil
 }
